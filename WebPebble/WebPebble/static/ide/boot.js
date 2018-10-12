6c19163ace508f8ccbb86e10667c05145514e91c
Roman-Port <darkwings220@gmail.com>
2018-10-12 15:51:54 -0500

    Fix log downloading bug.

--- WebPebble/WebPebble/static/ide/boot.js
+++ WebPebble/WebPebble/static/ide/boot.js
@@ -27,7 +27,7 @@ sidebarmanager.addButton("Compilation", 0, false, function () {
                 project.showDialog("Loading...", "Loading log.", [], [], null, false);
                 project.serverRequest(this.parentNode.x_data.api_log, function (data_d) {
                     project.hideDialog();
-                    project.displayLog(data_d.log, this.parentNode.x_data.id);
+                    project.displayLog(data_d.log, data_d.id);
                 }, null, true);
             });
             html_tools.createQuickDomClassEvent("Get PBW", 'div', b, 'med_button', function () {
