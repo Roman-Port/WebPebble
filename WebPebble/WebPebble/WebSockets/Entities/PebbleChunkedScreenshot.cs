6978ad0e5be86499c025a62a8d666362347c724f
Roman-Port <darkwings220@gmail.com>
2018-10-14 01:27:47 -0500

    More bitmap creation.

--- WebPebble/WebPebble/WebSockets/Entities/PebbleChunkedScreenshot.cs
+++ WebPebble/WebPebble/WebSockets/Entities/PebbleChunkedScreenshot.cs
@@ -9,13 +9,46 @@ namespace WebPebble.WebSockets.Entities
     public class PebbleChunkedScreenshot
     {
         public int chunkCount = 0;
+        public byte[] bmp_buffer;
+        public int buffer_pos = 0;
+
+        public int version;
+        public int width;
+        public int height;
 
         public AfterInterruptAction OnGotData(PebbleProtocolMessage msg)
         {
             Console.WriteLine("Got screenshot chunk of size " + msg.data.Length);
-            File.WriteAllBytes("/home/roman/tmp/chunk_" + chunkCount.ToString() + ".bin",msg.data);
+            if(chunkCount == 0)
+            {
+                //This is a header chunk. Open it.
+                using(MemoryStream ms = new MemoryStream(msg.data))
+                {
+                    ms.Position += 1;
+                    version = ReadInt32(ms);
+                    width = ReadInt32(ms);
+                    height = ReadInt32(ms);
+                    //Create the buffer based on the width and height.
+                    bmp_buffer = new byte[width * height];
+                    Console.WriteLine("(Debug) Created image of size " + width.ToString() + "x" + height.ToString());
+                    buffer_pos = 0;
+                }
+            } else {
+                //Copy this content to the buffer.
+                msg.data.CopyTo(bmp_buffer, buffer_pos);
+                buffer_pos += msg.data.Length;
+                Console.WriteLine("(Debug) Buffer completeness: " + buffer_pos.ToString() + "/" + bmp_buffer.Length.ToString());
+            }
             chunkCount++;
             return AfterInterruptAction.PreventDefault_ContinueInterrupt;
+        }
+
+        private int ReadInt32(MemoryStream ms)
+        {
+            byte[] buf = new byte[4];
+            ms.Read(buf, 0, 4);
+            Array.Reverse(buf);
+            return BitConverter.ToInt32(buf,0);
         }
     }
 }
