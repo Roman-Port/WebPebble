c1c4c26f5fb43d53cf6ab8e30b635ac4f99894a3
Roman-Port <darkwings220@gmail.com>
2018-10-14 02:07:05 -0500

    In theory, images can now be created

--- WebPebble/WebPebble/WebSockets/Entities/PebbleChunkedScreenshot.cs
+++ WebPebble/WebPebble/WebSockets/Entities/PebbleChunkedScreenshot.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Text;
 using static WebPebble.WebSockets.CloudPebbleDevice;
@@ -44,8 +46,56 @@ namespace WebPebble.WebSockets.Entities
                 buffer_pos += msg.data.Length;
                 Console.WriteLine("(Debug) Buffer completeness: " + buffer_pos.ToString() + "/" + bmp_buffer.Length.ToString());
             }
+            //If the buffer is complete, convert this into an actual image.
+            if(buffer_pos == bmp_buffer.Length)
+            {
+                Console.WriteLine("Buffer full. Creating image...");
+                FinalizeImage();
+            }
             chunkCount++;
             return AfterInterruptAction.PreventDefault_ContinueInterrupt;
+        }
+
+
+
+        public void FinalizeImage()
+        {
+            //Called after the buffer is complete.
+            bool oneBppMode = version == 1;
+            BitArray ba = new BitArray(bmp_buffer);
+
+            using(Bitmap bm = new Bitmap(width,height))
+            {
+                for(int x = 0; x<width; x++)
+                {
+                    for(int y = 0; y<height; y++)
+                    {
+                        int pos = (x * width) + y;
+
+                        if(oneBppMode)
+                        {
+                            //Read the bit instead of the byte.
+                            bool on = ba[pos];
+                            if (on)
+                                bm.SetPixel(x, y, Color.Black);
+                            else
+                                bm.SetPixel(x, y, Color.White);
+                        } else
+                        {
+                            //This is going to be slow....
+                            byte color_id = bmp_buffer[pos];
+                            //Convert this to an RGB color.
+                            PebbleColorMap value = (PebbleColorMap)color_id;
+                            string hex_code = "#" + value.ToString().Substring(1);
+                            var color = ColorTranslator.FromHtml(hex_code);
+                            bm.SetPixel(x, y, color);
+                        }
+                    }
+                }
+                //Save this image to RAM.
+                using (FileStream fs = new FileStream("/home/roman/test.png", FileMode.Create))
+                    bm.Save(fs, System.Drawing.Imaging.ImageFormat.Png);
+            }
         }
 
         private int ReadInt32(MemoryStream ms)
@@ -54,6 +104,74 @@ namespace WebPebble.WebSockets.Entities
             ms.Read(buf, 0, 4);
             Array.Reverse(buf);
             return BitConverter.ToInt32(buf,0);
+        }
+
+        enum PebbleColorMap
+        {
+            A000000,
+            A001e41,
+            A004387,
+            A0068ca,
+            A2b4a2c,
+            A27514f,
+            A16638d,
+            A007dce,
+            A5e9860,
+            A5c9b72,
+            A57a5a2,
+            A4cb4db,
+            A8ee391,
+            A8ee69e,
+            A8aebc0,
+            A84f5f1,
+            A4a161b,
+            A482748,
+            A40488a,
+            A2f6bcc,
+            A564e36,
+            A545454,
+            A4f6790,
+            A4180d0,
+            A759a64,
+            A759d76,
+            A71a6a4,
+            A69b5dd,
+            A9ee594,
+            A9de7a0,
+            A9becc2,
+            A95f6f2,
+            A99353f,
+            A983e5a,
+            A955694,
+            A8f74d2,
+            A9d5b4d,
+            A9d6064,
+            A9a7099,
+            A9587d5,
+            Aafa072,
+            Aaea382,
+            Aababab,
+            Aa7bae2,
+            Ac9e89d,
+            Ac9eaa7,
+            Ac7f0c8,
+            Ac3f9f7,
+            Ae35462,
+            Ae25874,
+            Ae16aa3,
+            Ade83dc,
+            Ae66e6b,
+            Ae6727c,
+            Ae37fa7,
+            Ae194df,
+            Af1aa86,
+            Af1ad93,
+            Aefb5b8,
+            Aecc3eb,
+            Affeeab,
+            Afff1b5,
+            Afff6d3,
+            Affffff,
         }
     }
 }
