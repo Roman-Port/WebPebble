cf70bf684d21f4b88fea301ff00cd3de7e008631
Roman-Port <darkwings220@gmail.com>
2018-10-21 17:35:38 -0500

    Attempt to create a valid HMAC for YCMD

--- WebPebble/WebPebble/WebSockets/ycmd/YcmdController.cs
+++ WebPebble/WebPebble/WebSockets/ycmd/YcmdController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace WebPebble.WebSockets.ycmd
@@ -12,6 +13,8 @@ namespace WebPebble.WebSockets.ycmd
         public const string YCMD_HOSTNAME = "localhost.localdomain";
         public const int YCMD_PORT = 43585;
 
+        public const string YCMD_SECRET = "";
+
         public static string GenerateUri(string pathname)
         {
             return "http://" + YCMD_HOSTNAME + ":" + YCMD_PORT.ToString() + pathname;
@@ -20,10 +23,16 @@ namespace WebPebble.WebSockets.ycmd
         public static T SendYcmdRequest<T>(string path, object requestData )
         {
             var request = (HttpWebRequest)WebRequest.Create(GenerateUri(path));
-            var data = Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(requestData));
+            string requestJson = JsonConvert.SerializeObject(requestData);
+            var data = Encoding.ASCII.GetBytes(requestJson);
             request.Method = "POST";
             request.ContentType = "application/json";
             request.ContentLength = data.Length;
+
+            //Add x-ycm-hmac header.
+            string hmac = GenerateHmac("POST", path, requestJson);
+            request.Headers.Add("x-ycm-hmac", hmac);
+            Console.WriteLine(hmac);
 
             using (var stream = request.GetRequestStream())
             {
@@ -33,6 +42,42 @@ namespace WebPebble.WebSockets.ycmd
             var jsonString = new StreamReader(r.GetResponseStream()).ReadToEnd();
 
             return JsonConvert.DeserializeObject<T>(jsonString);
+        }
+
+        public static string GenerateHmac(string method, string path, string body)
+        {
+            //Convert all to text
+            byte[] b_method = Encoding.UTF8.GetBytes(method);
+            byte[] b_path = Encoding.UTF8.GetBytes(path);
+            byte[] b_body = Encoding.UTF8.GetBytes(body);
+
+            byte[] joined;
+
+            using(MemoryStream ms = new MemoryStream())
+            {
+                WriteHmacToMs(b_method, ms);
+                WriteHmacToMs(b_path, ms);
+                WriteHmacToMs(b_body, ms);
+
+                ms.Position = 0;
+                joined = new byte[ms.Length];
+                ms.Read(joined, 0, joined.Length);
+            }
+
+            return Convert.ToBase64String(CalculateHmac(joined));
+        }
+
+        private static void WriteHmacToMs(byte[] data, Stream s)
+        {
+            byte[] buf = CalculateHmac(data);
+            s.Write(buf, 0, buf.Length);
+        }
+
+        private static byte[] CalculateHmac(byte[] data)
+        {
+            HMAC h = HMAC.Create();
+            h.Key = Encoding.UTF8.GetBytes(YCMD_SECRET);
+            return h.ComputeHash(data);
         }
     }
 }
