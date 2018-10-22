2b748872f1b5c4d0186db6f0dc96f59aff6311c4
Roman-Port <darkwings220@gmail.com>
2018-10-22 18:58:27 -0500

    Updated YCMD key. This shouldn't be used in production.

--- WebPebble/WebPebble/WebSockets/ycmd/YcmdController.cs
+++ WebPebble/WebPebble/WebSockets/ycmd/YcmdController.cs
@@ -13,7 +13,7 @@ namespace WebPebble.WebSockets.ycmd
         public const string YCMD_HOSTNAME = "localhost.localdomain";
         public const int YCMD_PORT = 43585;
 
-        public const string YCMD_SECRET = "5EjcGvUp5pvBOP4TkQuXhw==";
+        public const string YCMD_SECRET = "EL739LziGnBRxH3j";
 
         public static string GenerateUri(string pathname)
         {
