13fbe5c56a23f2d3ed4677d23fe00052186e3d36
Roman-Port <darkwings220@gmail.com>
2018-10-23 19:55:18 -0500

    YCMD Integration

---
+++ WebPebble/WebPebble/bin/Release/netcoreapp2.0/linux-x64/static/ide/code_complete.js
@@ -0,0 +1,31 @@
+﻿var ycmd = {};
+
+ycmd.subscribe = function () {
+    //Subscribe to events from the editor.
+    editor.on("change", ycmd.onEditorChange);
+};
+
+ycmd.onEditorChange = function (conte) {
+    //Get the position of the cursor.
+    var pos = editor.getCursorPosition();
+    pos.column += 1;
+    pos.row += 1;
+    var content = filemanager.loadedFiles[sidebarmanager.activeItem.internalId].session.getValue();
+    //Make a request to YCMD.
+    var data = {
+        "project_id": project.id,
+        "asset_id": sidebarmanager.activeItem.internalId,
+        "line_no": pos.row,
+        "col_no": pos.column,
+        "buffer": content
+    };
+    console.log(data);
+    //Create a request to YCMD.
+    phoneconn.send(6, data, function (ycmd_reply) {
+        console.log(ycmd_reply);
+    });
+};
+
+
+
+ycmd.subscribe();
