4b944ec9cb2fdd2d6af1a26cb3c5f78853cbf6ba
Roman-Port <darkwings220@gmail.com>
2018-10-24 20:29:34 -0500

    Scrapped much of the old ycmd client script. Rewriting parts of it.

--- WebPebble/WebPebble/bin/Release/netcoreapp2.0/linux-x64/static/ide/code_complete.js
+++ WebPebble/WebPebble/bin/Release/netcoreapp2.0/linux-x64/static/ide/code_complete.js
@@ -2,61 +2,24 @@
 ycmd.latestRequest = -1;
 ycmd.frame = document.getElementById('completion_frame');
 ycmd.open = false;
-ycmd.cursorPos = 0;
-ycmd.lastContent = "";
-
-ycmd.onEditCallback = null;
-ycmd.onEditTimeout = null;
+ycmd.filler_input = document.getElementById('completion_input');
 
 ycmd.subscribe = function () {
     //Subscribe to events from the editor.
     editor.on("change", ycmd.onEditorChange);
-    //Subscribe to keybinds.
-    keyboardJS.on('up', function () {
-        ycmd.onUpDown('up');
-    });
-    keyboardJS.on('down', function () {
-        ycmd.onUpDown('down');
-    });
-    keyboardJS.on('left', function () {
-        ycmd.hideBox();
-    });
-    keyboardJS.on('right', function () {
-        ycmd.hideBox();
-    });
-    keyboardJS.on('escape', function () {
-        ycmd.hideBox();
-    });
-    keyboardJS.on('enter', function () {
-        console.log("enter pressed");
-        if (ycmd.open) {
-            //Use the active one.
-            //Since we're unsure if we'll be called before the edit, we'll add this to the "on edit" callback. We'll also run this in 50 ms if that doesn't happen.
-            var f = function () {
-                clearTimeout(ycmd.onEditTimeout);
-                ycmd.onEditCallback = null;
-
-                ycmd.chooseOption(ycmd.frame.firstChild.x_complete[ycmd.boxPos].x_complete_data);
-            };
-            ycmd.onEditCallback = f;
-            ycmd.onEditTimeout = window.setTimeout(f, 50);
-        }
-    });
-    //Subscribe to clicks on the box.
-    document.getElementsByClassName('ace_content')[0].addEventListener('click', ycmd.hideBox);
+    //Subscribe to changes in the filler. These changes will just be mirrored in the input.
+    ycmd.filler_input.onchange = function () {
+        var d = this.value;
+        //Append this at the current position.
+        editor.insert(d);
+        //Debug
+        console.log("Redirected character typed: " + d);
+        //Clear.
+        this.value = "";
+    };
 };
 
 ycmd.onEditorChange = function (conte) {
-    //If there is a pending callback, run it.
-    if (ycmd.onEditCallback != null) {
-        ycmd.onEditCallback();
-        return;
-    }
-    //Set
-    ycmd.setSavedCursorPos();
-    var content = filemanager.loadedFiles[sidebarmanager.activeItem.internalId].session.getValue();
-    ycmd.lastContent = content;
-    
     //Get the position of the cursor.
     var pos = editor.getCursorPosition();
     pos.column += 1;
@@ -88,44 +51,7 @@ ycmd.onEditorChange = function (conte) {
 
 ycmd.onGotYcmdComp = function (data) {
     console.log(data);
-    //Create html for the predictions.
-    var e = document.createElement('div');
-    e.className = "completion_frame open_sans";
-    var ee = document.createElement('div');
-    ee.x_complete = [];
-    for (var i = 0; i < data.completions.length; i += 1) {
-        var o = document.createElement('div');
-        var d = data.completions[i];
-        console.log(d.type);
-        o.className = "c_item";
-        o.innerText = d.menu_text;
-
-        o.x_complete_data = d;
-        o.x_complete_id = i;
-
-        ee.appendChild(o);
-        ee.x_complete.push(o);
-    }
     
-    if (data.completions.length > 0) {
-        //Insert into DOM.
-        e.appendChild(ee);
-        //Start listening if there are things to listen for.
-        keyboardJS.watch(document.getElementsByClassName('ace_text-input')[0]);
-        ee.x_complete[0].className = "c_item c_item_select";
-        ycmd.boxPos = 0;
-        ycmd.open = true;
-        
-    } else {
-        //Hide.
-        ee = document.createElement('div');
-        ee.className = "completion_window completion_window_hidden";
-        e.appendChild(ee);
-        ycmd.open = false;
-    }
-
-    //Set position.
-    ycmd.setBoxPos(e);
 
     //Insert final
     ycmd.frame.parentNode.replaceChild(e, ycmd.frame);
@@ -143,9 +69,7 @@ ycmd.getBoxClassname = function (e) {
     } else {
         return "completion_window";
     }
-
-    
-}
+};
 
 ycmd.setBoxPos = function (e) {
     var f = e.firstChild;
@@ -168,75 +92,30 @@ ycmd.hideBox = function () {
     ycmd.open = false;
 };
 
-ycmd.onUpDown = function (key) {
-    //If this is open, set the cursor back and scroll through the dialog.
-    var pos = ycmd.cursorPos;
-    if (key == 'up') {
-        //editor.moveCursorTo(pos.row + 1, pos.column);
-        ycmd.setCursorPosInWindow(ycmd.boxPos - 1);
-    } else {
-        //editor.moveCursorTo(pos.row - 1, pos.column);
-        ycmd.setCursorPosInWindow(ycmd.boxPos + 1);
-    }
-    editor.moveCursorTo(pos.row, pos.column);
-};
+ycmd.showBox = function (options) {
+    //Create html for the predictions.
+    var e = document.createElement('div');
+    e.className = "completion_frame open_sans";
+    var ee = document.createElement('div');
+    ee.x_complete = [];
+    for (var i = 0; i < data.completions.length; i += 1) {
+        var o = document.createElement('div');
+        var d = data.completions[i];
+        console.log(d.type);
+        o.className = "c_item";
+        o.innerText = d.menu_text;
 
-ycmd.setCursorPosInWindow = function (newPos) {
-    var ee = ycmd.frame.firstChild;
-    //Check.
-    if (newPos < 0) {
-        newPos = 0;
-    }
-    if (newPos > ee.x_complete.length - 1) {
-        newPos = ee.x_complete.length - 1;
-    }
-    //Unset the existing one.
-    ee.x_complete[ycmd.boxPos].className = "c_item";
-    ee.x_complete[newPos].className = "c_item c_item_select";
-    ycmd.boxPos = newPos;
-    //Scroll to view.
-    ee.x_complete[ycmd.boxPos].scrollIntoView();
-};
+        o.x_complete_data = d;
+        o.x_complete_id = i;
 
-ycmd.chooseOption = function (data) {
-    //Get the current line.
-    var lines = ycmd.lastContent.toString().split('\n');
-    console.log(lines);
-    var cursorPos = ycmd.cursorPos;
-    var x = cursorPos.column;
-    var y = cursorPos.row;
-    //Work on our current line.
-    var l = lines[y];
-    var i = x;
-    //Work backwards to find the last space, or end of the line.
-    while (i > 0) {
-        if (i >= l.length) {
-            i -= 1;
-            continue;
-        }
-        if (l[i] == ' ') {
-            i += 1;
-            break;
-        }
-        //Remove this character.
-        l = l.slice(0, i) + l.slice(i+1);
+        ee.appendChild(o);
+        ee.x_complete.push(o);
     }
-    //Now, at the new position, insert this.
-    l = l.slice(0, i) + data.insertion_text + l.slice(i + 1);
-    //Set this back to a string.
-    lines[y] = l;
-    i = 0;
-    var o = "";
-    for (i = 0; i < lines.length; i += 1) {
-        o += lines[i] + "\n";
-    }
-    //Apply
-    filemanager.loadedFiles[sidebarmanager.activeItem.internalId].session.setValue(o, 0);
-    cursorPos.column += data.insertion_text.length;
-    window.setTimeout(function () {
-        editor.moveCursorTo(cursorPos.row, cursorPos.column);
-    }, 5);
-    editor.moveCursorTo(cursorPos.row, cursorPos.column);
-    //Hide the box
-    ycmd.hideBox();
-}
+
+    //Set box position and show it.
+    ycmd.setBoxPos(e);
+
+    //Take control.
+    ycmd.filler_input.value = "";
+    ycmd.filler_input.focus();
+};
