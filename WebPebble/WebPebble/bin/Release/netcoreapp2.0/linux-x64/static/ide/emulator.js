15031bb0f073abce38dc8d43b704f5ad68835f24
Roman-Port <darkwings220@gmail.com>
2018-10-26 20:53:26 -0500

    Style fixes.

--- WebPebble/WebPebble/bin/Release/netcoreapp2.0/linux-x64/static/ide/emulator.js
+++ WebPebble/WebPebble/bin/Release/netcoreapp2.0/linux-x64/static/ide/emulator.js
@@ -21,7 +21,7 @@ emu.launch = function (callback) {
         //Request to boot the emulator.
         emu.sendJson(1, { "platform": "basalt" });
         //Update status.
-        emu.setStatus("Emulator booting...", true);
+        emu.setStatus("Booting...", true);
     });
 };
 
@@ -35,7 +35,7 @@ emu.sendJson = function (type, data) {
 emu.setStatus = function (text, showLoader) {
     var area = document.getElementById('emu_loader');
     if (showLoader) {
-        text = '<img style="margin-right:10px;" src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjZweCIgIGhlaWdodD0iMjZweCIgIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgdmlld0JveD0iMCAwIDEwMCAxMDAiIHByZXNlcnZlQXNwZWN0UmF0aW89InhNaWRZTWlkIiBjbGFzcz0ibGRzLWVjbGlwc2UiIHN0eWxlPSJiYWNrZ3JvdW5kOiBub25lOyI+PHBhdGggbmctYXR0ci1kPSJ7e2NvbmZpZy5wYXRoQ21kfX0iIG5nLWF0dHItZmlsbD0ie3tjb25maWcuY29sb3J9fSIgc3Ryb2tlPSJub25lIiBkPSJNMTAgNTBBNDAgNDAgMCAwIDAgOTAgNTBBNDAgNDYgMCAwIDEgMTAgNTAiIGZpbGw9IiNlMTViNjQiIHRyYW5zZm9ybT0icm90YXRlKDIyMS44NzYgNTAgNTMpIj48YW5pbWF0ZVRyYW5zZm9ybSBhdHRyaWJ1dGVOYW1lPSJ0cmFuc2Zvcm0iIHR5cGU9InJvdGF0ZSIgY2FsY01vZGU9ImxpbmVhciIgdmFsdWVzPSIwIDUwIDUzOzM2MCA1MCA1MyIga2V5VGltZXM9IjA7MSIgZHVyPSIxcyIgYmVnaW49IjBzIiByZXBlYXRDb3VudD0iaW5kZWZpbml0ZSI+PC9hbmltYXRlVHJhbnNmb3JtPjwvcGF0aD48L3N2Zz4=" style="padding-top:3px; padding-bottom:3px; float:left; height:24px;"><div style="float:left;">' + text + '</div>';
+        text = '<div style="float:left;">'+text+'</div><img style="margin-right:10px;" src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjZweCIgIGhlaWdodD0iMjZweCIgIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgdmlld0JveD0iMCAwIDEwMCAxMDAiIHByZXNlcnZlQXNwZWN0UmF0aW89InhNaWRZTWlkIiBjbGFzcz0ibGRzLWVjbGlwc2UiIHN0eWxlPSJiYWNrZ3JvdW5kOiBub25lOyI+PHBhdGggbmctYXR0ci1kPSJ7e2NvbmZpZy5wYXRoQ21kfX0iIG5nLWF0dHItZmlsbD0ie3tjb25maWcuY29sb3J9fSIgc3Ryb2tlPSJub25lIiBkPSJNMTAgNTBBNDAgNDAgMCAwIDAgOTAgNTBBNDAgNDYgMCAwIDEgMTAgNTAiIGZpbGw9IiNlMTViNjQiIHRyYW5zZm9ybT0icm90YXRlKDIyMS44NzYgNTAgNTMpIj48YW5pbWF0ZVRyYW5zZm9ybSBhdHRyaWJ1dGVOYW1lPSJ0cmFuc2Zvcm0iIHR5cGU9InJvdGF0ZSIgY2FsY01vZGU9ImxpbmVhciIgdmFsdWVzPSIwIDUwIDUzOzM2MCA1MCA1MyIga2V5VGltZXM9IjA7MSIgZHVyPSIxcyIgYmVnaW49IjBzIiByZXBlYXRDb3VudD0iaW5kZWZpbml0ZSI+PC9hbmltYXRlVHJhbnNmb3JtPjwvcGF0aD48L3N2Zz4=" style="padding-top:3px; padding-bottom:3px; float:left; height:24px;">';
     }
     area.innerHTML = "<div style=\"line-height:20px;\">" + text + "</div>";
     //Make sure this is what the scroller is set to.
@@ -67,6 +67,6 @@ emu.onEmulatorLaunch = function(data) {
     //Prepare view
 
     //Call final callback
-    ws.onLoadCallback();
-    ws.onLoadCallback = null;
+    emu.ws.onLoadCallback();
+    emu.ws.onLoadCallback = null;
 };
