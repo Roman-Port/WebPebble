4d2f589c767ac183c56e5cc9bbe23821891f52e4
Roman-Port <darkwings220@gmail.com>
2018-11-24 15:37:09 -0600

    Added a real login method using RPWS OAUTH2.

--- WebPebble/WebPebble/Program.cs
+++ WebPebble/WebPebble/Program.cs
@@ -50,6 +50,9 @@ namespace WebPebble
             AddService(false, Services.CreateProject.CreateProject.OnRequest, "/create", true);
             AddService(false, Services.QuickLogin.Serve, "/token/", false);
             AddService(false, Services.ProjectList.Serve, "/myprojects", true);
+
+            AddService(false, Services.LoginService.BeginLogin, "/login", true);
+            AddService(false, Services.LoginService.FinishLogin, "/complete_login", true);
             //Add services that are in the project.
             AddService(true, Services.Projects.FileManager.OnRequest, "/media/", true);
             AddService(true, Services.Projects.FileManager.UploadFile, "/upload_media/", true);
---
+++ WebPebble/WebPebble/Services/Login.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace WebPebble.Services
+{
+    public static class LoginService
+    {
+        public static void BeginLogin(Microsoft.AspNetCore.Http.HttpContext e, Oauth.E_RPWS_User user, Entities.WebPebbleProject non_proj)
+        {
+            //Redirect the user to the RPWS oauth screen.
+            string returnUri = "https://api.webpebble.get-rpws.com/complete_login";
+            if (e.Request.Query.ContainsKey("return"))
+                returnUri += $"?return={System.Web.HttpUtility.UrlEncode(e.Request.Query["return"])}";
+            else
+                returnUri += $"?return={System.Web.HttpUtility.UrlEncode("https://webpebble.get-rpws.com/")}";
+
+            string redir = $"https://blue.api.get-rpws.com/v1/oauth2/?returnuri={System.Web.HttpUtility.UrlEncode(returnUri)}";
+            e.Response.Headers.Add("Location", redir);
+            Program.QuickWriteToDoc(e, $"You should've been redirected to {redir}.", "text/html", 302);
+        }
+
+        public static void FinishLogin(Microsoft.AspNetCore.Http.HttpContext e, Oauth.E_RPWS_User user, Entities.WebPebbleProject non_proj)
+        {
+            //Called when the user finishes the RPWS oauth. Get the token from the endpoint.
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(e.Request.Query["endpoint"]);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            string payload;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                payload = reader.ReadToEnd();
+            }
+
+            //Decode payload
+            RpwsPayload p = JsonConvert.DeserializeObject<RpwsPayload>(payload);
+
+            if (p.ok == false)
+                throw new Exception($"Failed to authenticate with RPWS; {p.message}");
+
+            //Get the token and set it as a cookie. We'll worry about csrf before release.
+            string token = p.access_token;
+
+            e.Response.Cookies.Append("access-token", token, new Microsoft.AspNetCore.Http.CookieOptions
+            {
+                Expires = new DateTimeOffset(10, 0, 0, 0, 0, 0, TimeSpan.Zero),
+                IsEssential = true,
+                Path = "/"
+            });
+
+            //Redirect to the redirect path.
+            string redir = e.Request.Query["return"];
+            e.Response.Headers.Add("Location", redir);
+            Program.QuickWriteToDoc(e, $"You should've been redirected to {redir}.", "text/html", 302);
+        }
+
+        class RpwsPayload
+        {
+            public bool ok;
+            public string message;
+            public string access_token;
+        }
+    }
+}
