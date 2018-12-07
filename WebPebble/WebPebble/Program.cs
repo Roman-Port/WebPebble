40dacdbb771c347b94bad07e8f76054940947d69
Roman-Port <darkwings220@gmail.com>
2018-12-06 23:04:41 -0600

    Fixed a whoopsie.

--- WebPebble/WebPebble/Program.cs
+++ WebPebble/WebPebble/Program.cs
@@ -57,7 +57,7 @@ namespace WebPebble
             AddService(false, Services.LoginService.BeginLogin, "/login", false);
             AddService(false, Services.LoginService.FinishLogin, "/complete_login", false);
             //Add services that are in the project.
-            AddService(true, Services.Projects.FileManager.OnMediaCreateRequest, "/media/", true);
+            AddService(true, Services.Projects.FileManager.OnMediaRequest, "/media/", true);
             AddService(true, Services.Projects.FileManager.UploadFile, "/upload_media/", true);
             AddService(true, Services.Projects.FileManager.AppInfoJson, "/appinfo.json", true);
             AddService(true, Services.Projects.FileManager.OnProjSettingsRequest, "/settings/", true);
